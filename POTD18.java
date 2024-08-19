f623c92e636500d405b35fe416d254180c902feb
mathsphile <ndas20997@gmail.com>
2024-08-19 18:43:39 +0530

    POTD 19/08/24
    Kth Smallest

---
+++ POTD18.java
@@ -0,0 +1,47 @@
+
+//Given an array arr[] and an integer k where k is smaller than the size of the array, the task is to find the kth smallest element in the given array. It is given that all array elements are distinct.
+
+// Follow up: Don't solve it using the inbuilt sort function.
+
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+        return quickSelect(arr, 0, arr.length - 1, k - 1);
+    }
+
+    private static int quickSelect(int[] arr, int low, int high, int k) {
+        if (low == high) {
+            return arr[low];
+        }
+
+        int pivotIndex = partition(arr, low, high);
+
+        if (pivotIndex == k) {
+            return arr[pivotIndex];
+        } else if (pivotIndex > k) {
+            return quickSelect(arr, low, pivotIndex - 1, k);
+        } else {
+            return quickSelect(arr, pivotIndex + 1, high, k);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr, i, high);
+        return i;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+    }
+}
