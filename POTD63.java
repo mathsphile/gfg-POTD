dc1667dd48c428dd54be726bca24f7a72436c561
mathsphile <ndas20997@gmail.com>
2024-10-23 11:01:37 +0530

    POTD 23/10/24

---
+++ POTD63.java
@@ -0,0 +1,36 @@
+//Given a single linked list, calculate the sum of the last n nodes.
+
+//Note: It is guaranteed that n <= number of nodes.
+
+class Solution {
+
+    // Return the sum of last n nodes
+    public int sumOfLastN_Nodes(Node head, int n) {
+        // Initialize variables
+        Node temp = head;
+        int length = 0;
+
+        // Step 1: Calculate the total length of the linked list
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+
+        // Step 2: Traverse to the (length - n)th node
+        int skip = length - n;
+        temp = head;
+        while (skip > 0) {
+            temp = temp.next;
+            skip--;
+        }
+
+        // Step 3: Calculate the sum of the last n nodes
+        int sum = 0;
+        while (temp != null) {
+            sum += temp.data;
+            temp = temp.next;
+        }
+
+        return sum;
+    }
+}
