95050d3034e3c488858a3e786aeac11fa788d567
mathsphile <ndas20997@gmail.com>
2024-11-21 11:48:47 +0530

    POTD 21/11/24

---
+++ POTD86.java
@@ -0,0 +1,22 @@
+//The cost of stock on each day is given in an array price[]. Each day you may decide to either buy or sell the stock at price[i], you can even buy and sell the stock on the same day. Find the maximum profit that you can get.
+
+//Note: A stock can only be sold if it has been bought previously and multiple stocks cannot be held on any given day. POTD86 {
+    
+class Solution {
+    public int maximumProfit(int prices[]) {
+        // Initialize profit to 0
+        int profit = 0;
+
+        // Loop through the array to calculate profit
+        for (int i = 1; i < prices.length; i++) {
+            // If the price of the current day is greater than the previous day
+            if (prices[i] > prices[i - 1]) {
+                // Add the difference to profit
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        // Return the total profit
+        return profit;
+    }
+}
